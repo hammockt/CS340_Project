ce9ee298a6a8e34b7665703cc6fe04af1b11c816
hammockt <hammocktrevor@gmail.com>
2018-06-10 01:48:22 -0700

    Remove a lot of duplicate code

--- API/deleteBuildCPUCooler.php
+++ API/deleteBuildCPUCooler.php
@@ -1,49 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Content-Type: application/json");
 
-require_once 'phpUtilities.php';
-require_once 'restUtilities.php';
-require_once 'getUtilities.php';
-require_once 'postUtilities.php';
-require_once 'tokenUtilities.php';
-require_once 'vendor/autoload.php';
-
-use Lcobucci\JWT\Parser;
-
-//make sure they are getting this endpoint
-$httpMethods = ["POST"];
-enforceHttpMethods($httpMethods);
-
-$data = getJsonFromHttpBody();
-
-//make sure that they sent us a authToken
-$requiredKeys = ['authToken', 'id'];
-$optionalKeys = [];
-enforceKeys($data, $requiredKeys, $optionalKeys);
-enforceNonEmptyKeys($data, $requiredKeys);
-
-$authToken = $data['authToken'];
-$id        = $data['id'];
-
-validateInteger($id);
-
-//to token string into a token object
-$token = (new Parser())->parse($authToken);
-$config = loadConfig();
-verifyAuthToken($config, $token);
-
-$pdo = new PDO($config['db_dsn'], $config['db_user'], $config['db_password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-$query = 'CALL deleteBuildCPUCooler(:id)';
-$statement = $pdo->prepare($query);
-$statement->bindValue(':id', $id, PDO::PARAM_INT);
-$statement->execute();
-
-$affectedRows = $statement->fetchObject()->affectedRows;
-$output = (object) [ 'affectedRows' => $affectedRows ];
-
-printf("%s", json_encode($output));
+$table = 'BuildCPUCooler';
+require_once 'deleteBuildPart.php';
 
 ?>
